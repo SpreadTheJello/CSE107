083b52c65ca5be788edb05933f105cc088f4ef22
SpreadTheJello <28422680+SpreadTheJello@users.noreply.github.com>
2022-10-24 21:04:20 -0700

    Lab 3 - Complete

--- Lab 3/MyImageFunctions.py
+++ Lab 3/MyImageFunctions.py
@@ -1,15 +1,24 @@
 import numpy as np
 import math
 
-# Write the functionmyimresize() that takes as input
-# A numpy matrix representing a grayscale image.
-# The number of rows in the resized image.
-# The number of columns in the resized image.
-# A string with values ‘nearest’ or ‘bilinear’.
-# and outputs the resized numpy matrix image.
-# Based off the TA's sample file
 def myImageResize(inImage_pixels, M, N, interpolation_method ):
-
+# This function will resize the image based on the pixels and the interpolation method for grayscaled images.
+# Syntax:
+#   up_down_sampled_im_bilinear_pixels = myImageResize(upsampled_im_bilinear_pixels, 400, 400, 'bilinear')
+#
+# Input: 
+#    inImage_pixels = numpy matrix of grayscale image 
+#    M = number of rows 
+#    N = number of columns 
+#    interpolation_method = 'nearest' or 'bilinear' method
+#
+# Output: 
+#    out = Resized numpy matrix image
+#
+# History:
+#   A. Fatali   10/23/22     Created    
+#
+# Based off the TA's sample file
     Minput, Ninput = inImage_pixels.shape
     # create a new output matrix
     out = np.zeros(shape=(M, N))
@@ -75,18 +84,47 @@ def myImageResize(inImage_pixels, M, N, interpolation_method ):
     return out
 
 def mybilinear(x1,y1,p1,x2,y2,p2,x3,y3,p3,x4,y4,p4,x5,y5,p5):
+# This function preforms bilinear interpolation 
+# Syntax:
+#   p5 = mybilinear(x1,y1,p1,x2,y2,p2,x3,y3,p3,x4,y4,p4,x5,y5,p5)
+#
+# Input: 
+#   x = the location in x
+#   y = the location in y
+#   p = the pixel value
+#
+# Output:
+#   p5 = the pixel value
+#
+# History:
+#   A. Fatali   10/22/22     Created    
+#
+# Based off the TA's sample file
     # use eq from hw
     # apply eq on p1 and p3  -> t1
     # apply eq on p2 and p4  -> t2
     # ....        t1 and t2  -> p5
-
     p5_prime = (p3-p1)*((x5-x1)/(x3-x1))+p1
     p5_doubleprime = (p4-p2)*((x5-x2)/(x4-x2))+p2
     p5 = (p5_doubleprime-p5_prime)*((y5-y1)/(y2-y1))+p5_prime
     return p5
 
 def myRMSE(first_im_pixels, second_im_pixels):
-
+# This function is the root mean squared error (RMSE). It compares matrix of two images.
+# Syntax:
+#   up_down_bilinear_RMSE = myRMSE(first_im_pixels, sec_im_pixels)
+#
+# Input: 
+#   first_im_pixels = the original image matrix
+#   sec_im_pixels = the second image matrix
+#
+# Output:
+#   sqrt(total) = The root mean squared error
+#
+# History:
+#   A. Fatali   10/22/22     Created    
+#
+# Based off the TA's sample file
     total = 0
     M,N = first_im_pixels.shape
     for m in range(0,M):
